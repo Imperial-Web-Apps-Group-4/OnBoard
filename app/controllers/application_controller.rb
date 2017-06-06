86535eb20137e52bd65655387d94d2626fbb7eee
Ashley Davies-Lyons <ashley@davies.me.uk>
2017-06-06 18:36:46 +0100

    Implemented basic user functionality - registering, logging in, logging out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :check_login
+
+  private
+    def check_login
+      return unless session[:logged_in_email]
+
+      @current_user = User.find_by_email(session[:logged_in_email])
+    rescue ActiveRecord::RecordNotFound
+      session[:logged_in_email] = nil
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root :to => 'games#index'
+  resources :users, :path_names => { :new => 'register' } do
+    collection do
+      get 'login' => 'users#login'
+      post 'login' => 'users#login_attempt'
+      get 'logout' => 'users#logout'
+    end
+  end
   resources :games do
     resources :game_sessions, :path => 'sessions', :except => ['index'],
               :path_names => { :edit => 'play' }, :param => 'game_hash'
